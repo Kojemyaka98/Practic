11a0ebcbb2b8afbb827a928cab5f5a566770b752
Kojemyaka98 <akitin98@gmail.com>
2020-08-22 17:00:02 +0300

    this is first commit in new project

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+'use strict' // эта директива обозначает что мы работаем с ES6
+
+let arr = ['plum.png', 'growfood.jpg'];
+console.log(arr[1]);
+
+//alert('GrowFood');
+
+// let answer = confirm('Здесь будет вопрос?');
+// console.log(answer);
+ let answer = prompt ("Do you are 18 years old?", "Yes");
+ console.log(typeof(arr));
